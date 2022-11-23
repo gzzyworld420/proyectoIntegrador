8a27c18fe0d30b7c369a009658270d25a70f9e46
gzzyworld420 <iaracena@udesa.edu.ar>
2022-11-23 20:26:48 -0300

    chequeando rutas

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -6,7 +6,10 @@ const indexController = require("../controllers/indexController");
 
 // metodo
 router.get('/', indexController.index);
-
+//agregando rutas - -> Benito, chequeate esto que este bien despues :)
+router.get('/search/usuarios', indexController.searchUsuarios)
+router.get('/search', indexController.search);
+router.get('/search/user', indexController.searchUser);
 
 
 router.get('/searchResults', indexController.searchResults);
