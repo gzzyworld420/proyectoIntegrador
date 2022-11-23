daef665fd04c4e5c74f24a649e2a2b51d5faaac8
gzzyworld420 <iaracena@udesa.edu.ar>
2022-11-23 18:01:07 -0300

    indexController verificado, modificado y terminado

--- myApp/controllers/indexController.js
+++ myApp/controllers/indexController.js
@@ -1,53 +1,108 @@
 // require
+let db = require('../database/models')
 const db = require('../data/data');
-//const posts = db.posts
 
-const dB = require("../database/models");
+const {
+    where,
+    EmptyResultError
+} = require('sequelize');
 const posteos = dB.Posteo;
+let comentario = db.comentario;
 const users = dB.User;
+let op = db.sequelize.Op;
 
-//const op = db.sequelice.Op
-
-
+// prueba cambios en el indexController
 // methods
 const indexController = {
-
-    
     index: (req, res) => {
-       
+
         let criterios = {
-            include: [{ all: true, nested: true }, {association: "users"}, {association: "comments"}],
+            include: [{
+                all: true,
+                nested: true
+            }, {
+                association: "users"
+            }, {
+                association: "comments"
+            }],
             limit: 20,
-            order: [['created_at', 'DESC']]
+            order: [
+                ['created_at', 'DESC']
+            ]
 
         };
-
         posteos.findAll(criterios, {
-            include: [ {association: "users"}, {association: "comments"}]
+            include: [{
+                association: "users"
+            }, {
+                association: "comments"
+            }]
         })
-        .then((results) => {
-            return res.render("index", { posts : results}
-            )
-        })
-
-        .catch(error => {
-            console.log(error);
-        })
-        
-        
-       //return res.render("index", { posts : db, comments : db} );
 
     },
-    
-    searchResults: (req, res) => {
-        res.render('searchResults', { posts : db, comments : db} );
+    searchUsuarios: (req, res) => {
+        return res.render('formSearchUsuarios')
     },
-
-
-
+    searchUser: (req, res) => {
+        buscado = req.query.buscadoUsuario;
+        let criterios = {
+            include: [{
+                all: true,
+                nested: true
+            }],
+            where: [{
+                nombre: {
+                    [op.like]: "%" + buscado + "%"
+                }
+            }],
+            limit: 10,
+            order: [
+                ['created_at', 'DESC']
+            ]
+        }
+        usuario.findAll(criterios)
+            .then(function (resultado) {
+                return res.render('resUserSearch', {
+                    post: db,
+                    comments: db
+                })
+            })
+    },
+    search: (req, res) => {
+        buscado = req.query.buscado;
+        console.log(req.query.order);
+        let primerCriterio = {
+            include: [{ all: true, nested: true }],
+            where: [{ nombre: { [op.like]: "%" + buscado + "%" } }],
+            limit: 10,
+            order: [['created_at', 'DESC']]
+        }
+        let segundocriterio = {
+            include: [{ all: true, nested: true }],
+            where: [{ nombre: { [op.like]: "%" + buscado + "%" } }],
+            limit: 10,
+            order: [['created_at', 'ASC']]
+        }
+        if (req.query.order != null) {
+            posteo.findAll(segundoCriterio)
+            .then(function (resultado) {
+                return res.render('searchResult', { post: db, comments: db})
+            })
+            .catch(function (error) {
+                return res.send(error)
+            })
+            
+        } else {
+            posteo.findAll(primerCriterio)
+            .then(function (resultado) {
+                return res.render('searchResult', { post: db, comments: db })
+            
+            })
+            .catch(function (error) {
+                return res.send(error)
+            })
+        }
+    }
 }
-
-
 // export
-
 module.exports = indexController;
